eda4c2585831543d4c5deb9cdfc2cc85e478904b
kyoungduck.park <kyoungduck.park@kt.com>
2018-11-12 13:53:03 +0900

    config server first commit

---
+++ config/GW-Config/src/main/java/com/kdpark/gwconfig/GwConfigApplication.java
@@ -0,0 +1,12 @@
+package com.kdpark.gwconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwConfigApplication.class, args);
+	}
+}
