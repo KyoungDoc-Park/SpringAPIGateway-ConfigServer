5c79f812b812b77a7d0e6f6d1d3ba04ce6c29b93
kyoungduck.park <kyoungduck.park@kt.com>
2018-11-12 17:27:05 +0900

    config 설정 변경

--- config/GW-Config/src/main/java/com/kdpark/gwconfig/GwConfigApplication.java
+++ config/GW-Config/src/main/java/com/kdpark/gwconfig/GwConfigApplication.java
@@ -1,12 +1,28 @@
 package com.kdpark.gwconfig;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @SpringBootApplication
 public class GwConfigApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GwConfigApplication.class, args);
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+	      SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+          sessionFactory.setDataSource(dataSource);          
+          Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");          
+          sessionFactory.setMapperLocations(res);    
+          return sessionFactory.getObject();
 	}
 }
---
+++ config/GW-Config/src/main/resources/db/script.sql
@@ -0,0 +1,21 @@
+
+CREATE TABLE route_properties (
+	application		VARCHAR(50)	NOT NULL
+	, profile		VARCHAR(50)	NOT NULL
+	, label			VARCHAR(50)	NOT NULL
+	, group_key		VARCHAR(50)	NOT NULL
+	, group_type		VARCHAR(50)	NOT NULL
+	, group_value		VARCHAR(500)	NOT NULL
+
+	, CONSTRAINT route_properties PRIMARY KEY (group_key, group_type)
+)
+
+
+-- insert sample
+INSERT INTO fiveGProperties
+	(application, profile, label, group_key, group_type, group_value)
+VALUES
+	('gateway', 'default', 'master', 'auth', 'path', '/auth/**'),
+	('gateway', 'default', 'master', 'auth', 'url', 'http://localhost:8888'),
+	('gateway', 'default', 'master', 'auth', 'path', '/cms/**'),
+	('gateway', 'default', 'master', 'auth', 'url', 'http://localhost:9999');
